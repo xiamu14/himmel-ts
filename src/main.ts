6136ee0aab2b83ad9e482a5d63ff3a6b00b2f5ff
ben <x_z_gfree@163.com>
2024-02-17 22:35:22 +0800

    feature: update himmel demo feature.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createRoot } from "himmel";
-import { Div, Img, Text } from "himmel/dom";
+import { Button, Div, Img, Portal, Text } from "himmel/dom";
 import { dispatch, get, signal } from "himmel/signal";
 import "virtual:uno.css";
 import himmelLogo from "./himmel-logo.svg";
 import "./style.css";
 import viteLogo from "./typescript.svg";
 const countSignal = signal(0);
+const hideSignal = signal(true);
+
 function App() {
   return Div([
     Div([
@@ -21,10 +23,33 @@ function App() {
           dispatch(countSignal, (old) => old + 1);
         })
     ).className("card"),
+    Button("show modal").onClick(() => {
+      dispatch(hideSignal, false);
+    }),
     Text("Click on the Himmel and TypeScript logos to learn more").className(
       "read-the-docs"
     ),
   ]);
 }
+
+Portal(
+  Div(
+    Div(
+      Div(
+        Div(
+          "With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply."
+        ).className("p-4 md:p-5 space-y-4")
+      ).className("relative bg-white rounded-lg shadow")
+    ).className("relative p-4 w-full max-w-2xl max-h-full m-auto")
+  )
+    .className(
+      "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-100vw md:inset-0 h-[calc(100%-1rem)] h-full bg-gray-700/[0.8] flex"
+    )
+    .hide(() => get(hideSignal))
+    .onClick(() => {
+      dispatch(hideSignal, true);
+    }),
+  document.querySelector("body") as HTMLElement
+);
 
 createRoot("#app", App);
